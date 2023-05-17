445ccaea6ab38829789eff9ee97d1aa9a2f5fd0a
thujuli <thujuli037@gmail.com>
2023-05-17 10:10:16 +0800

    Initial commit

---
+++ .config/nvim/lua/configs/colorscheme/init.lua
@@ -0,0 +1,7 @@
+require('configs.colorscheme.catppuccin')
+require('configs.colorscheme.nord')
+require('configs.colorscheme.tokyonight')
+
+-- vim.cmd([[colorscheme tokyonight-storm]])
+-- vim.cmd[[colorscheme nord]]
+vim.cmd.colorscheme "catppuccin"
